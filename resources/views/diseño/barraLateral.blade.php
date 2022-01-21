9daf385eeaad81af7090ca90e5998ccc9fa6d6b0
Jorge Luis <jorgestudio2017@gmail.com>
2022-01-20 23:05:25 -0400

    modificando plantillas

--- resources/views/diseño/barraLateral.blade.php
+++ resources/views/diseño/barraLateral.blade.php
@@ -1,35 +1,33 @@
 
 
 <nav id="sidebar">
-
-  <div class="sidebar-header">
-    <h3>!Bienvenida!</h3>
-    <h5> {{Auth::user()->personas->nombre}} {{Auth::user()->personas->apellidos}}</h5>
-  </div>
-  <ul class="lisst-unstyled components">
-    <p>MENU</p>
-    @if (Auth::user()->personas->tipo == 'A')
+    <div class="sidebar-header">
+        <h3>!Bienvenida!</h3>
+        <h5> {{Auth::user()->personas->nombre}} {{Auth::user()->personas->apellidos}}</h5>
+    </div>
+    <ul class="lisst-unstyled components">
+        <p>MENU</p>
+        @if (Auth::user()->personas->tipo == 'A')
+        <li>
+            <a href="{{ route('personal.index') }}"><span><i class="fa fa-lock" style="margin-left:-5px;"></i></span> Personal</a>
+        </li>
+        @endif
     <li>
-      <a href="{{ route('personal.index') }}"><span><i class="fa fa-lock" style="margin-left:-5px;"></i></span> Personal</a>
-    </li>
-    @endif
-    <li>
-      <a href="#repuesto" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+        <a href="#repuesto" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
         <span><i class="fa fa-home" style="margin-left:-5px;"></i></span> Repuestos</a>
-      <ul class="collapse list-unstyled" id="repuesto">
-      @if (Auth::user()->personas->tipo == 'A')
+        <ul class="collapse list-unstyled" id="repuesto">
+        @if (Auth::user()->personas->tipo == 'A')
         <li>
           <a href="{{ route('categoria.create') }}">Categorías</a>
         </li>
         <li>
           <a href="{{ route('marca.create') }}">Marcas</a>
         </li>
-      @endif
+        @endif
         <li>
           <a href="{{ route('repuesto.index') }}">Repuestos</a>
         </li>
-
-      </ul>
+        </ul>
     </li>
 
     <li>
@@ -43,7 +41,7 @@
       <li>
           <a href="{{ route('pago.create') }}">Tipo de Pagos</a>
       </li>
-      @endif 
+      @endif
       <li>
           <a href="{{ route('cliente.index') }}">Clientes</a>
       </li>
@@ -53,7 +51,7 @@
       <li>
          <a href="{{ route('reserva.index') }}">Reservas</a>
       </li>
-      </ul>
+        </ul>
     </li>
     <li>
       <a href="#pedidos" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
@@ -70,50 +68,5 @@
 
       </ul>
     </li>
-     
-    <li>
-      <a href="#"><span><i class="fa fa-info-circle" style="margin-left:-5px;"></i></span> About</a>
-    </li>
-    <li>
-      <a href="#course" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-        <span><i class="fa fa-book" style="margin-left:-5px;"></i></span> Courses</a>
-      <ul class="collapse list-unstyled" id="course">
-        <li>
-          <a href="#">HTML 5</a>
-        </li>
-        <li>
-          <a href="#">CSS 3</a>
-        </li>
-        <li>
-          <a href="#">Bootstrap 4</a>
-        </li>
-        <li>
-          <a href="#">Javascript & Jquery</a>
-        </li>
-        <li>
-          <a href="#">Anguler.Js</a>
-        </li>
-
-
-      </ul>
-    </li>
-    <li>
-      <a href="#Career" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-        <span><i class="fa fa-graduation-cap" style="margin-left:-5px;"></i></span> Career</a>
-      <ul class="collapse list-unstyled" id="Career">
-        <li>
-          <a href="#">Front-End Developer</a>
-        </li>
-        <li>
-          <a href="#">Back-Hand Developer</a>
-        </li>
-
-      </ul>
-    </li>
-
-    <li>
-      <a href="#"><span><i class="fa fa-phone" style="margin-left:-5px;"></i></span> Contact Us</a>
-    </li>
-
   </ul>
 </nav>
--- resources/views/diseño/contenido.blade.php
+++ resources/views/diseño/contenido.blade.php
@@ -10,64 +10,39 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
-
 <body>
-
-
 	<div class="wrapper">
+        @include('diseño.barraLateral')
+        <div id="content">
 
-	@include('diseño.barraLateral')
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <button type="button" id="sidebarCollapse" class="btn  btn-outline-info">
+                            <i class="fa fa-align-left"></i>
+                        </button>
+                    </div>
+                </nav>
 
-		<div id="content">
-
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<div class="container-fluid">
-					<button type="button" id="sidebarCollapse" class="btn  btn-outline-info">
-						<i class="fa fa-align-left"></i>
-					</button>
-				</div>
-			</nav>
-
-      <ul class="nav justify-content-end">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="{{route('logout')}}">Salir</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#">Disabled</a>
-  </li>
-</ul>
-
-			<br>
-
-      @yield('contenido')
-
-
-		</div>
-
-
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('logout')}}">Salir</a>
+                </li>
+            </ul>
+            <br>
+        @yield('contenido')
+        </div>
 	</div>
-
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <script src="{{ asset('js/validacion.js') }}"></script>
-	<script>
-
+    <script src="{{ asset('js/validacion.js') }}"></script>
+    <script>
 		$(document).ready(function () {
 			$('#sidebarCollapse').on('click', function () {
 				$('#sidebar').toggleClass('active');
 			});
 		});
-
 	</script>
-
-
 </body>
 
 </html>
